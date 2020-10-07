38b84751f9f42c390e1a8443399b0069216fc0c8
Amaury <amaury.meugnier@hotmail.fr>
2020-10-07 10:10:51 +0200

    dossier avec routes

--- js/projet.js
+++ js/projet.js
@@ -1,4 +1,34 @@
+const accueil = {
+    template: `<div></div>`
+};
+const boutique = {
+    template: `<div></div>`
+};
+const livre_or = {
+    template: `<div></div>`
+};
+const formulaire_contact = {
+    template: `<div></div>`
+};
+
+var routes = [
+    { path: '/accueil', component: accueil },
+    { path: '/boutique', component: boutique },
+    { path: '/livre-or', component: livre_or },
+    { path: '/formulaire', component: formulaire_contact },
+
+];
+
+const router = new VueRouter({
+    routes: routes
+});
+
+
+
+
 var vm = new Vue({
-    el: '#app',
+    el: "#app",
     data: {},
+    methods: {},
+    router: router
 });
